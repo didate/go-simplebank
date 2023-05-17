9de7418d7df6f1e0b5b8807690b1135003a53fc3
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2023-05-17 15:46:33 +0000

    Load config file with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/didate/simple_bank/util"
 	_ "github.com/lib/pq"
 )
 
@@ -16,8 +17,11 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config :", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db :", err)
 	}
